410b22f8482de1a133b9ceca780a55b226c49312
Life4gal <life4gal@gmail.com>
2026-08-31 15:03:03 +0800

    core/engine/series_registry: - 字段代码现在是枚举类型(屏蔽整数类型支持的运算) - 支持使用字段代码查询是否已注册 - 支持使用字段代码查询字段名

--- core/engine/series_registry.cpp
+++ core/engine/series_registry.cpp
@@ -3,18 +3,29 @@
 #include <unordered_map>
 
 #include <core/utility/string_hash.hpp>
+#include <core/utility/enum.hpp>
 
 namespace
 {
 	using namespace cg;
 
-	std::unordered_map<std::string, engine::series_code_type, utility::StringHash, std::equal_to<>> g_registered_series;
-	engine::series_code_type g_series_code_generator = 1;
+	class SeriesCodeWrapper : public utility::Enum<
+				engine::SeriesCode,
+				// +, +=, -, -=
+				utility::EnumSupport::ARITHMETIC
+			> {};
+
+	constexpr auto series_code_begin = static_cast<engine::SeriesCode>(1);
+	constexpr auto series_code_step = static_cast<engine::SeriesCode>(1);
+
+	std::unordered_map<std::string, engine::SeriesCode, utility::StringHash, std::equal_to<>> g_registered_series;
+	std::unordered_map<engine::SeriesCode, std::string_view> g_registered_series_name;
+	SeriesCodeWrapper g_series_code_generator{series_code_begin};
 }
 
 namespace cg::engine
 {
-	auto SeriesRegistry::register_series(const std::string_view series_name) noexcept -> series_code_type
+	auto SeriesRegistry::register_series(const std::string_view series_name) noexcept -> SeriesCode
 	{
 		if (const auto it = g_registered_series.find(series_name);
 			it != g_registered_series.end())
@@ -22,16 +33,34 @@ namespace cg::engine
 			return it->second;
 		}
 
-		const auto next_series_code = g_series_code_generator;
-		g_series_code_generator += 1;
+		const auto next_series_code = static_cast<SeriesCode>(g_series_code_generator);
+		g_series_code_generator += series_code_step;
 
 		const auto [it, _] = g_registered_series.emplace(series_name, next_series_code);
+		g_registered_series_name.emplace(it->second, it->first);
+
 		return it->second;
 	}
 
-	auto SeriesRegistry::registered(const std::string_view series_name) noexcept -> series_code_type
+	auto SeriesRegistry::registered(const std::string_view series_name) noexcept -> bool
 	{
 		return g_registered_series.contains(series_name);
+	}
+
+	auto SeriesRegistry::registered(const SeriesCode code) noexcept -> bool
+	{
+		return g_registered_series_name.contains(code);
+	}
+
+	auto SeriesRegistry::series_name(const SeriesCode code) noexcept -> std::string_view
+	{
+		if (const auto it = g_registered_series_name.find(code);
+			it != g_registered_series_name.end())
+		{
+			return it->second;
+		}
+
+		return {};
 	}
 
 	auto SeriesRegistry::size() noexcept -> std::size_t
--- core/engine/series_registry.hpp
+++ core/engine/series_registry.hpp
@@ -4,16 +4,22 @@
 
 namespace cg::engine
 {
-	using series_code_type = std::uint32_t;
+	enum class SeriesCode : std::uint32_t {};
 
 	class SeriesRegistry
 	{
 	public:
 		// 注册字段(如果还未注册),返回字段代码
-		[[nodiscard]] static auto register_series(std::string_view series_name) noexcept -> series_code_type;
+		[[nodiscard]] static auto register_series(std::string_view series_name) noexcept -> SeriesCode;
 
 		// 检查字段是否已注册
-		[[nodiscard]] static auto registered(std::string_view series_name) noexcept -> series_code_type;
+		[[nodiscard]] static auto registered(std::string_view series_name) noexcept -> bool;
+
+		// 检查字段代码是否已注册
+		[[nodiscard]] static auto registered(SeriesCode code) noexcept -> bool;
+
+		// 获取字段名称
+		[[nodiscard]] static auto series_name(SeriesCode code) noexcept -> std::string_view;
 
 		// 获取已注册字段数量
 		[[nodiscard]] static auto size() noexcept -> std::size_t;
